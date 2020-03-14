f738f30e4f5dc623dd129090d8ece50feef7e83f
Odi Wan Kenobi <alexander.labrosse@gmail.com>
2020-03-14 15:42:28 -0400

    Create Install-PowerShell7.ps1

---
+++ PowerShell 7/Install-PowerShell7.ps1
@@ -0,0 +1,16 @@
+## ExecutionPolicy
+Set-ExecutionPolicy RemoteSigned -Force
+Get-ExecutionPolicy -List
+
+## Download
+$url = "https://github.com/PowerShell/PowerShell/releases/download/v7.0.0/PowerShell-7.0.0-win-x64.msi"
+$output = "$env:USERPROFILE\Downloads\PowerShell-7-x64.msi"
+Invoke-WebRequest -Uri $url -OutFile $output
+
+## Install
+.\$output
+
+## Profile
+Test-Path $PROFILE
+if Test-Path $PROFILE -eq $false
+New-item –Type File -Force $PROFILE
