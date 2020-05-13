7be8e3d46b7ee00d8e87f5e4c676859fdc21b1ce
Alex Labrosse <alexander.labrosse@gmail.com>
2020-05-13 13:41:26 -0400

    Azure
    Azure

--- Azure/Azure-Server.ps1
+++ Azure/Azure-Server.ps1
@@ -1,19 +1,49 @@
-<#
-    ## Windows 10 (Desktop / Client)
-        Set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server' -name "fDenyTSConnections" -value 0
-        Enable-NetFirewallRule -DisplayGroup "Remote Desktop"
+## Chocolatey
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iwr https://chocolatey.org/install.ps1 -UseBasicParsing | iex
 
-    ## Windows Server
-        (Get-WmiObject Win32_TerminalServiceSetting -Namespace root\cimv2\TerminalServices).SetAllowTsConnections(1,1) | Out-Null
-        (Get-WmiObject -Class "Win32_TSGeneralSetting" -Namespace root\cimv2\TerminalServices -Filter "TerminalName='RDP-tcp'").SetUserAuthenticationRequired(0) | Out-Null
-        Get-NetFirewallRule -DisplayName "Remote Desktop*" | Set-NetFirewallRule -enabled true
-#>
+## Chocolatey Settings
+choco feature enable -n allowGlobalConfirmation
+choco feature enable -n useRememberedArgumentsForUpgrades
 
-## Determine OS Type
+## Boxstarter
+. { iwr -useb https://boxstarter.org/bootstrapper.ps1 } | iex; Get-Boxstarter -Forceiex "& { $(irm https://aka.ms/install-powershell.ps1) } -UseMSI"
+
+## Timezone
+Set-TimeZone -Name "Eastern Standard Time"
+
+## SMBv1
+Disable-WindowsOptionalFeature -Online -FeatureName smb1protocol
+
+## Remoting
+Enable-PSRemoting -Force -ErrorAction 0
+Set-WSManQuickConfig -Force -ErrorAction 0
+
+## Script(s)
+Install-Script Test-PendingReboot -Force
+
+## PowerShell 7
+iex "& { $(irm https://aka.ms/install-powershell.ps1) } -UseMSI"
+
+## Software
+choco install notepadplusplus
+choco install googlechrome
+choco install chocolateygui
+choco install 7zip
+
+## VS Code
+choco install vscode
+choco install vscode-powershell
+choco install vscode-intellicode
+
+## Updates
+Enable-MicrosoftUpdate
+Install-WindowsUpdate -getUpdatesFromMS -acceptEula -SuppressReboots
+
+## Determine OS Type - RDP
 $osType = Get-CimInstance -ClassName Win32_OperatingSystem
 $osType.ProductType | Out-Null
 
-## Write Output to Shell
+## Write Output to Shell - RDP
 if ($osType.ProductType -eq 1) {
     Set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server' -name "fDenyTSConnections" -value 0
     Enable-NetFirewallRule -DisplayGroup "Remote Desktop"
