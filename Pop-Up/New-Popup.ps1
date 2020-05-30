6a4f54f8f99dff808b8acecc1215d9948e1a39ad
Alex Labrosse <alexander.labrosse@gmail.com>
2020-05-30 07:35:06 -0400

    Create New-Popup.ps1

---
+++ Pop-Up/New-Popup.ps1
@@ -0,0 +1,13 @@
+<#
+    .TITLE
+        'New-Popup.ps1'
+    .AUTHOR
+        Alex Labrosse
+    .DESCRIPTION
+        Invokes a basic pop-up message, with title, message, and Yes/No answer returns.
+    .REFERENCES
+        http://woshub.com/popup-notification-powershell/
+#>
+
+$wshell = New-Object -ComObject Wscript.Shell
+$Output = $wshell.Popup("Remember to stay hydradated.`nHave you drunk water recently?",0,"Hydrate!",4+32)
