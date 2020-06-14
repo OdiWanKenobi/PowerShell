825e0fc533bc0962effc061fbfe3ee5a64788408
Alex Labrosse <alexander.labrosse@gmail.com>
2020-06-14 09:48:06 -0400

    Update Bind-Computer.ps1

--- Scripts/Bind-Computer.ps1
+++ Scripts/Bind-Computer.ps1
@@ -5,10 +5,20 @@
         Takes the serial number, and uses the last 6 digits to generate a new hostname.
 #>
 
-## Variables
-$SerialNumber = Get-WmiObject Win32_Bios | Select-Object -ExpandProperty SerialNumber
-$NewHostName = "NY" + $SerialNumber.Substring(1,6)
-$DomainName = Read-Host "Enter the AD domain name"
-Add-Computer -NewName $NewHostName -DomainName $DomainName
+## Using Serial Number to Create New Host Name
+$sn = Get-WmiObject Win32_Bios | Select-Object -ExpandProperty SerialNumber
+$snHostName = "UESFL2CP" + $sn.Substring(1,4)
 
+## Domain-Join Credentials and Array
+$creds = Get-Credential -Prompt "Enter domain credentials"
+$args = @{
+    NewName = $snHostName
+    Server = "UESFL2DC01"
+    Domain = "sysadmin.lan"
+    Restart = $true
+    Confirm = $false
+    Force = $true
+}
 
+## Binding to Domain
+Add-Computer @args
