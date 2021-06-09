1af3f23edfbe3aa655b9b0d72db6c9d2fcd74883
Alex Labrosse <alexander.labrosse@gmail.com>
2021-06-09 01:28:58 -0400

    Create Purge-UpdateCache.ps1

---
+++ PSWindowsUpdates/Purge-UpdateCache.ps1
@@ -0,0 +1,21 @@
+# Stopping Windows Update Services
+Get-Service *wuauserv* | Stop-Service -Force -Confirm:$false
+Get-Service *crypSvc* | Stop-Service -Force -Confirm:$false
+Get-Service *bits* | Stop-Service -Force -Confirm:$false
+Get-Service *msiserver* | Stop-Service -Force -Confirm:$false
+
+# Deleting Windows Update Cache Folders in "SoftwareDistribution"
+# C:\Windows\SoftwareDistribution\Download
+Get-ChildItem "C:\Windows\SoftwareDistribution\Download" -Force | Remove-Item -Recurse -Confirm:$false
+
+# C:\Windows\SoftwareDistribution\DataStore
+Get-ChildItem "C:\Windows\SoftwareDistribution\DataStore" -Force | Remove-Item -Recurse -Confirm:$false
+
+# C:\Windows\SoftwareDistribution\PostRebootEventCache.V2
+Get-ChildItem "C:\Windows\SoftwareDistribution\PostRebootEventCache.V2" -Force | Remove-Item -Recurse -Confirm:$false
+
+# Starting Windows Update Services
+Get-Service *wuauserv* | Start-Service -Force
+Get-Service *crypSvc* | Start-Service -Force
+Get-Service *bits* | Start-Service -Force
+Get-Service *msiserver* | Start-Service -Force
